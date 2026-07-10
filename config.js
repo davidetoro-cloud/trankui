6eb167b2496e7e8eac0c564bfcc8913e6085643c
davidetoro-cloud <davidetoro@gmail.com>
2026-07-10 23:36:08 +0200

    Update config.js

--- config.js
+++ config.js
@@ -4,8 +4,3 @@ window.TRANKUI_CONFIG = {
   siteUrl: "https://trankui.com",
   vapidPublicKey: "BBDNfFK1mFHULR_2cT5TIzk4l9gqecKsC_TXBa50N0SabZ1I_KUgah_La7bzlIgPcEoC2ajNnC4rWe9OWbiBOO4",
 };
-window.TRANKUI_CONFIG = {
-  supabaseUrl: "https://oehqbflkhxyuimdnmaqh.supabase.co",
-  supabasePublishableKey: "sb_publishable_jdLJjXSUON38vTRui_WFmA_hL0wkwNO",
-  siteUrl: "https://trankui.com",
-};
